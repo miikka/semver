3dfa3bb6da4817cc0abc6b9dc755ba5e8b638691
Miikka Koskinen <miikka.koskinen@iki.fi>
2022-01-19 08:46:33 +0200

    Initialize project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojurs.miikka/semver "0.1.0-SNAPSHOT"
+  :description "semver parser for Clojure"
+  :url "https://github.com/miikka/semver"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns semver.core})
