43c142ed4f40ae2b975c702ed121acb1e23e3613
Miikka Koskinen <miikka.koskinen@iki.fi>
2022-01-22 20:24:38 +0200

    feat: implement the primitive operators

--- src/semver/core.clj
+++ src/semver/core.clj
@@ -5,7 +5,7 @@
 
 (defn parse-version [version]
   (->> (str/split version #"\.")
-       (map #(Integer/valueOf %))))
+       (mapv #(Integer/valueOf %))))
 
 (deftest parse-version-test
   (is (= [1 0 0] (parse-version "1.0.0"))))
@@ -16,26 +16,13 @@
 
 (deftest parse-range-test
   (is (= [:= [1 0 0]] (parse-range "1.0.0")))
+  (is (= [:= [1 0 0]] (parse-range "=1.0.0")))
   (is (= [:>= [1 0 0]] (parse-range ">=1.0.0"))))
 
-(defmulti satisfies-range? (fn [_version range] (first range)))
+(def operator-kw->fn {:< <, :> >, :<= <=, :>= >=, := =})
 
-(defmethod satisfies-range? := [version [_ range-version]]
-  (= version range-version))
-
-;; TODO(miikka) Replace the code below with... just using compare on the version vectors
-
-(defmethod satisfies-range? :>= [version [_ range-version]]
-  (->> (map compare version range-version)
-       (drop-while zero?)
-       (first)
-       ((fnil nat-int? 0))))
-
-(defmethod satisfies-range? :> [version [_ range-version]]
-  (->> (map compare version range-version)
-       (drop-while zero?)
-       (first)
-       ((fnil pos? 0))))
+(defn satisfies-range? [version [operator range-version]]
+  ((operator-kw->fn operator) (compare version range-version) 0))
 
 (defn satisfies?
   "Returns true if version satisfies range."
@@ -45,6 +32,8 @@
 (deftest satisfies?-test
   (is (satisfies? "1.0.0" "1.0.0"))
   (is (not (satisfies? "1.0.0" "2.0.0")))
+  (is (satisfies? "1.0.0" "=1.0.0"))
+  (is (not (satisfies? "1.0.0" "=2.0.0")))
   (is (satisfies? "1.0.0" ">=1.0.0"))
   (is (satisfies? "1.0.0" ">=0.1.0"))
   (is (not (satisfies? "1.0.0" ">=1.0.1")))
@@ -53,4 +42,8 @@
   (is (satisfies? "2.0.0" ">1.0.0"))
   (is (satisfies? "1.0.1" ">1.0.0"))
   (is (satisfies? "0.0.1" ">0.0.0"))
-  (is (not (satisfies? "1.0.0" ">1.0.0"))))
+  (is (not (satisfies? "1.0.0" ">1.0.0")))
+  (is (satisfies? "0.1.0" "<1.0.0"))
+  (is (not (satisfies? "1.0.0" "<1.0.0")))
+  (is (satisfies? "1.0.0" "<=1.0.0"))
+  (is (not (satisfies? "1.0.0" "<=0.1.0"))))
