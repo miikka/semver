6a07f1178fbbac05a8aa400eea1c06d5e26e865d
Miikka Koskinen <miikka@metosin.fi>
2022-01-26 06:26:33 +0000

    feat: implement the caret operator

--- src/semver/core.clj
+++ src/semver/core.clj
@@ -4,14 +4,16 @@
             [clojure.test :refer [deftest is]]))
 
 (defn parse-version [version]
-  (->> (str/split version #"\.")
-       (mapv #(Integer/valueOf %))))
+  (when version
+    (->> (str/split version #"\.")
+         (mapv #(Integer/valueOf %)))))
 
 (deftest parse-version-test
+  (is (= nil (parse-version nil)))
   (is (= [1 0 0] (parse-version "1.0.0"))))
 
 (defn parse-range [range]
-  (let [[_ operator version] (re-matches #"(<|<=|>|>=|=)?([0-9.]+)" range)]
+  (let [[_ operator version] (re-matches #"(<|<=|>|>=|=|\^)?([0-9.]+)" range)]
     [(if operator (keyword operator) :=) (parse-version version)]))
 
 (deftest parse-range-test
@@ -21,13 +23,34 @@
 
 (def operator-kw->fn {:< <, :> >, :<= <=, :>= >=, := =})
 
+(defn expand-range [[operator version :as range]]
+  (cond
+    (contains? operator-kw->fn operator) [range]
+    (= (keyword "^") operator)
+    (let [[_ upper] (reduce (fn [[bumped? upper] component]
+                          (if (or bumped? (zero? component))
+                            [bumped? (conj upper 0)]
+                            [true (conj upper (inc component))]))
+                        [false []]
+                        version)]
+      [[:>= version]
+       [:< upper]])))
+
+(deftest expand-range-test
+  (is (= [[:= [1 2 3]]] (expand-range [:= [1 2 3]])))
+  (is (= [[:>= [1 2 3]], [:< [2 0 0]]] (expand-range [(keyword "^") [1 2 3]])))
+  (is (= [[:>= [0 2 3]], [:< [0 3 0]]] (expand-range [(keyword "^") [0 2 3]])))
+  (is (= [[:>= [0 0 3]], [:< [0 0 4]]] (expand-range [(keyword "^") [0 0 3]]))))
+
 (defn satisfies-range? [version [operator range-version]]
   ((operator-kw->fn operator) (compare version range-version) 0))
 
 (defn satisfies?
   "Returns true if version satisfies range."
   [version range]
-  (satisfies-range? (parse-version version) (parse-range range)))
+  (let [parsed-version (parse-version version)
+        expanded-ranges (expand-range (parse-range range))]
+    (every? (partial satisfies-range? parsed-version) expanded-ranges)))
 
 (deftest satisfies?-test
   (is (satisfies? "1.0.0" "1.0.0"))
@@ -47,3 +70,20 @@
   (is (not (satisfies? "1.0.0" "<1.0.0")))
   (is (satisfies? "1.0.0" "<=1.0.0"))
   (is (not (satisfies? "1.0.0" "<=0.1.0"))))
+
+(deftest satisfies?-caret-test
+  (is (satisfies? "1.2.3" "^1.2.3"))
+  (is (satisfies? "1.3.3" "^1.2.3"))
+  (is (satisfies? "1.2.4" "^1.2.3"))
+  (is (not (satisfies? "1.2.2" "^1.2.3")))
+  (is (not (satisfies? "2.0.0" "^1.2.3")))
+  (is (satisfies? "0.2.3" "^0.2.3"))
+  (is (satisfies? "0.2.4" "^0.2.3"))
+  (is (not (satisfies? "0.2.2" "^0.2.3")))
+  (is (not (satisfies? "1.2.3" "^0.2.3")))
+  (is (not (satisfies? "0.3.3" "^0.2.3")))
+  (is (satisfies? "0.0.3" "^0.0.3"))
+  (is (not (satisfies? "0.0.2" "^0.0.3")))
+  (is (not (satisfies? "0.0.4" "^0.0.3")))
+  (is (not (satisfies? "0.1.0" "^0.0.3")))
+  (is (not (satisfies? "1.0.0" "^0.0.3"))))
