a5d095db87fdb5e027288e2ef89c3891596e90f4
Miikka Koskinen <miikka.koskinen@iki.fi>
2022-04-23 13:37:27 +0300

    chore: upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/miikka/semver"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.60.977"]]}}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.65.1029"]]}}
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
   :repl-options {:init-ns semver.core})
